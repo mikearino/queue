9632dbc1a78a05426bc6f5d09c2fc888183e0535
Mike Lofgren <mikeylofgren@gmail.com>
2019-09-08 16:25:54 -0700

    add 404 not found route:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,17 @@ import TicketList from './TicketList';
 import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 import NewTicketForm from './NewTicketForm';
+import Error404 from './Error404';
 
-
-function App(){
+function App(props){
+  console.log(props)
   return (
     <div>
       <Header/>
       <Switch>
         <Route exact path="/" component={TicketList} />
         <Route path='/newticket' component={NewTicketForm} />
+        <Route component={Error404} />
       </Switch>
     </div>
   );
